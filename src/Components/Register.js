0c479058f47c875cfca7859eb1f9e1321289f6c3
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-04-16 11:04:49 +0530

    added classNames

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,14 +1,25 @@
 import React, { Fragment } from 'react';
-
+import './CssComp/style.css';
 export const Register = () => {
 	return (
 		<Fragment>
-			<h2>Register</h2>
-			<form action="">
-				<input type="text" placeholder="Username" />
-				<input type="email" placeholder="Email" />
-				<input type="password" placeholder="Password" />
-			</form>
+			<div className="registerForm">
+				<div className="title">
+					<h2>Register</h2>
+				</div>
+
+				<form action="">
+					<input type="text" placeholder="Username" />
+					<input type="email" placeholder="Email" />
+					<input type="password" placeholder="Password" />
+					<button type="submit" className="btnRegister">
+						Register
+					</button>
+					<p>
+						Already have an account? <a> Sign in</a>
+					</p>
+				</form>
+			</div>
 		</Fragment>
 	);
 };
